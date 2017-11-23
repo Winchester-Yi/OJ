185dec742116757e30ea9a803dc6f013f8da53a5
Winchester-Yi <619589959@qq.com>
2017-11-23 16:59:06 +0800

    modify 1010.c

--- patest_basiclevel/1010.c
+++ patest_basiclevel/1010.c
@@ -5,18 +5,22 @@
     > Created Time: Wed 22 Nov 2017 03:17:30 PM CST
  ************************************************************************/
 
-#include<stdio.h>
-
-int main(){
-	int a[2];
-	int i=0;
-	while(scanf("%d",&a[i])==1){
-		if(i!=1)
-			i++;
-		else{
-			i=0;
-			printf("%d %d\n",a[0]*a[1],a[1]-1);
-		}
-	}
-	return 0;
+#include <stdio.h>
+#include <string.h>
+int main()
+{
+    int n, e, flag = 0;
+    while (scanf("%d%d", &n, &e) != EOF)
+    {
+        if (n*e)
+        {
+            if (flag)
+                printf(" ");
+            else
+                flag = 1;
+            printf("%d %d", n*e, e - 1);
+        }
+    }
+    if (!flag) printf("0 0");
+    return 0;
 }
