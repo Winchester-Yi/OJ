babcce82a9b09658efc2e87a4a36252508d03be8
Winchester-Yi <619589959@qq.com>
2017-12-07 17:17:37 +0800

    modify 1019

--- patest_basiclevel/1019.c
+++ patest_basiclevel/1019.c
@@ -1,58 +1,30 @@
-/*************************************************************************
-    > File Name: 1019.c
-    > Author: YiShixiang
-    > Mail: yishixiang@gmail.com 
-    > Created Time: Wed 06 Dec 2017 03:22:37 PM CST
- ************************************************************************/
-
-#include<stdio.h>
-#include<stdlib.h>
-
-struct r* sort(char *s);
-
-int main()
-{
-	char n[6];
-	struct r{
-		int small;
-		int large;
-	}*re;
-	int sub;
-
-	scanf("%s",n);
-	if((n-n)==0){
-		printf("%d - %d = %4d",atoi(n),atoi(n),0);
-		return 0;
-	}
-	do{
-		re=sort(n);
-		sub=(re->large)-(re->small);
-		printf("%d - %d = %d",re->large,re->small,sub);
-	}while((sub!=6174)&&(sub!=0));
-	printf("%d - %d = %d",re->large,re->small,sub);
-	return 0;
-}
-
-struct r* sort(const char *s)
-{
-	struct r* re;
-	char pesu[6];
-	char temp;
-	int i,j;
-
-	strcpy(pesu,s);
-	for(i=0;i<strlen(pesu);i++){
-		for(j=i+1;j<strlen(pesu);j++){
-			if(pesu[i]>pesu[j]){
-				temp = pesu[i];
-				pesu[i] = pesu[j];
-				pesu[j] = temp;
-			}
-		}
-	}
-	
-	re->small=atoi(pesu);
-	re->large=atoi(strrev(pesu));
-	return re;
-}
-
+#include <stdio.h>  
+#include <algorithm>  
+using namespace std;  
+int inc, de;  
+void gao(int x){  
+    int a[4];  
+    a[0] = x / 1000;  
+    a[1] = x / 100 % 10;  
+    a[2] = x / 10 % 10;  
+    a[3] = x % 10;  
+    sort(a, a + 4);  
+    inc = a[0] * 1000 + a[1] * 100 + a[2] * 10 + a[3];  
+    de = a[3] * 1000 + a[2] * 100 + a[1] * 10 + a[0];  
+}  
+  
+int main(){  
+    int n, s;  
+    scanf("%d", &n);  
+    if ((n / 1000 == n / 100 % 10) && (n / 1000 == n / 10 % 10) && (n / 1000 == n % 10)){  
+        printf("%04d - %04d = 0000\n", n, n);  
+        return 0;  
+    }  
+    do{  
+        gao(n);  
+        s = de - inc;  
+        printf("%04d - %04d = %04d\n", de, inc, s);  
+        n = s;  
+    } while (s != 6174);  
+    return 0;  
+}  
